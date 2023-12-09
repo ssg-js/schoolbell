8da8b31e497e921aaccfadc10704ce4706dc8bda
ssg-js <76690497+ssg-js@users.noreply.github.com>
2023-12-10 00:40:13 +0900

    docs: 과제폴더 생성

---
+++ 과제2/solve2.js
@@ -0,0 +1,21 @@
+// 여기에 정답을 작성해주세요
+
+
+
+
+// 아래 코드는 수정하지 않습니다
+
+// 1
+$('#target-1').removeClass('border');
+
+// 2
+$('#target-1').css('left', '250px');
+
+// 3
+$('.target-2').removeClass('border').addClass('blue');
+
+// 4
+$('.target-2').css({ 'left': 50, 'margin-top': '-15px' })
+
+// 5
+$('#target-3').fadeOut(() => $('#target-4').addClass('green'));
